c9973ea63bd46a141386b47815fbf62e346cb9b2
Krit Grover <105399404+kritgrover@users.noreply.github.com>
2023-05-31 20:52:52 -0400

    Updated Experience method to resolve issues
    - Returns specific experience
    - Returns experience list
    - Adds experience

--- app.py
+++ app.py
@@ -44,13 +44,35 @@ def experience():
     '''
     Handle experience requests
     '''
+
     if request.method == 'GET':
-        return jsonify()
+        index = request.args.get("index")
+        if index is not None:
+            index = int(index)
+            if 0 <= index < len(data["experience"]) - 1:
+                exp = data["experience"][index]
+                return jsonify({"title": exp.title,
+                                "company": exp.company,
+                                "start_date": exp.start_date,
+                                "end_date": exp.end_date,
+                                "description": exp.description,
+                                "logo": exp.logo,
+                                })
+        return jsonify(data["experience"])
 
     if request.method == 'POST':
-        return jsonify({})
+        req = request.get_json()
+        new = Experience(req["title"],
+            req["company"],
+            req["start_date"],
+            req["end_date"],
+            req["description"],
+            req["logo"]
+        )
 
-    return jsonify({})
+        data["experience"].append(new)
+        return jsonify({"id": data["experience"].index(new)})
+
 
 @app.route('/resume/education', methods=['GET', 'POST'])
 def education():
