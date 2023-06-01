532272cbbb4103e98b2d56903e79860b56469354
Krit Grover <105399404+kritgrover@users.noreply.github.com>
2023-05-31 21:21:12 -0400

    Update Education and Skill to resolve issues
    GET requests:
      - Returns specific education and skill
      - Returns all educations and skills
    
    POST requests:
      - Adds new education or skill and returns index

--- app.py
+++ app.py
@@ -80,12 +80,32 @@ def education():
     Handles education requests
     '''
     if request.method == 'GET':
-        return jsonify({})
+        index = request.args.get("index")
+        if index is not None:
+            index = int(index)
+            if 0 <= index < len(data["education"]) - 1:
+                edu = data["education"][index]
+                return jsonify({"course": edu.course,
+                                "school": edu.school,
+                                "start_date": edu.start_date,
+                                "end_date": edu.end_date,
+                                "grade": edu.grade,
+                                "logo": edu.logo,
+                                })
+        return jsonify(data["education"])
 
     if request.method == 'POST':
-        return jsonify({})
+        req = request.get_json()
+        new = Education(req["course"],
+            req["school"],
+            req["start_date"],
+            req["end_date"],
+            req["grade"],
+            req["logo"]
+        )
 
-    return jsonify({})
+        data["education"].append(new)
+        return jsonify({"id": data["education"].index(new)})
 
 
 @app.route('/resume/skill', methods=['GET', 'POST'])
@@ -94,9 +114,23 @@ def skill():
     Handles Skill requests
     '''
     if request.method == 'GET':
-        return jsonify({})
+        index = request.args.get("index")
+        if index is not None:
+            index = int(index)
+            if 0 <= index < len(data["skill"]) - 1:
+                ski = data["skill"][index]
+                return jsonify({"name": ski.name,
+                                "proficiency": ski.proficiency,
+                                "logo": ski.logo,
+                                })
+        return jsonify(data["skill"])
 
     if request.method == 'POST':
-        return jsonify({})
+        req = request.get_json()
+        new = Skill(req["name"],
+            req["proficiency"],
+            req["logo"]
+        )
 
-    return jsonify({})
+        data["skill"].append(new)
+        return jsonify({"id": data["skill"].index(new)})
