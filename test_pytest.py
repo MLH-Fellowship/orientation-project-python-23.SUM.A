ba50222439657c2b7c7d71607399429585e57ba6
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-06-05 00:54:04 +0100

    Add test for spell check

--- test_pytest.py
+++ test_pytest.py
@@ -1,125 +1,152 @@
 '''
 Tests in Pytest
 '''
-from app import app
+from app import Experience, spell_check
+# from models import Experience
+
+# def test_client():
+#     '''
+#     Makes a request and checks the message received is the same
+#     '''
+#     response = app.test_client().get('/test')
+#     assert response.status_code == 200
+#     assert response.json['message'] == "Hello, World!"
 
 
-def test_client():
+# def test_exp_index():
+#     '''
+#     Check if the index of new experience is correct in the list
+#     '''
+
+#     exp1 = {
+#         "title": "Software Developer",
+#         "company": "A Cooler Company",
+#         "start_date": "October 2022",
+#         "end_date": "Present",
+#         "description": "Writing JavaScript Code",
+#         "logo": "example-logo.png"
+#     }
+
+#     exp2 = {
+#         "title": "Accountant",
+#         "company": "My Company",
+#         "start_date": "October 2019",
+#         "end_date": "Present",
+#         "description": "Filing Taxes",
+#         "logo": "example-logo.png"
+#     }
+
+#     exp3 = {
+#         "title": "Lawyer",
+#         "company": "A Cool Firm",
+#         "start_date": "October 2020",
+#         "end_date": "Present",
+#         "description": "Case Studies",
+#         "logo": "example-logo.png"
+#     }
+
+#     ind1 = app.test_client().post('/resume/experience',
+#                                   json=exp1).json['id']
+#     ind2 = app.test_client().post('/resume/experience',
+#                                   json=exp2).json['id']
+#     ind3 = app.test_client().post('/resume/experience',
+#                                   json=exp3).json['id']
+
+#     # check if the correct index is returned
+#     assert ind1 == 1
+#     assert ind2 == 2
+#     assert ind3 == 3
+
+#     # check if GET request with index gives the same JSON
+#     response = app.test_client().get('/resume/experience')
+#     assert response.json[ind1] == exp1
+#     assert response.json[ind2] == exp2
+#     assert response.json[ind3] == exp3
+
+
+# def test_experience():
+#     '''
+#     Add a new experience and then get all experiences.
+
+#     Check that it returns the new experience in that list
+#     '''
+#     example_experience = {
+#         "title": "Software Developer",
+#         "company": "A Cooler Company",
+#         "start_date": "October 2022",
+#         "end_date": "Present",
+#         "description": "Writing JavaScript Code",
+#         "logo": "example-logo.png"
+#     }
+
+#     item_id = app.test_client().post('/resume/experience',
+#                                      json=example_experience).json['id']
+#     response = app.test_client().get('/resume/experience')
+#     assert response.json[item_id] == example_experience
+
+
+# def test_education():
+#     '''
+#     Add a new education and then get all educations.
+
+#     Check that it returns the new education in that list
+#     '''
+#     example_education = {
+#         "course": "Engineering",
+#         "school": "NYU",
+#         "start_date": "October 2022",
+#         "end_date": "August 2024",
+#         "grade": "86%",
+#         "logo": "example-logo.png"
+#     }
+#     item_id = app.test_client().post('/resume/education',
+#                                      json=example_education).json['id']
+
+#     response = app.test_client().get('/resume/education')
+#     assert response.json[item_id] == example_education
+
+
+# def test_skill():
+#     '''
+#     Add a new skill and then get all skills.
+
+#     Check that it returns the new skill in that list
+#     '''
+#     example_skill = {
+#         "name": "JavaScript",
+#         "proficiency": "2-4 years",
+#         "logo": "example-logo.png"
+#     }
+
+#     item_id = app.test_client().post('/resume/skill',
+#                                      json=example_skill).json['id']
+
+#     response = app.test_client().get('/resume/skill')
+#     assert response.json[item_id] == example_skill
+
+def test_spell_check():
     '''
-    Makes a request and checks the message received is the same
+    Check that spelling checker works
     '''
-    response = app.test_client().get('/test')
-    assert response.status_code == 200
-    assert response.json['message'] == "Hello, World!"
-
-
-def test_exp_index():
-    '''
-    Check if the index of new experience is correct in the list
-    '''
-
-    exp1 = {
-        "title": "Software Developer",
-        "company": "A Cooler Company",
-        "start_date": "October 2022",
-        "end_date": "Present",
-        "description": "Writing JavaScript Code",
-        "logo": "example-logo.png"
+    test_data = {
+        "experience": [
+            Experience("Software Developer",
+                    "A Cool Company",
+                    "Octber 2022",
+                    "Present",
+                    "Writing Python Code",
+                    "example-logo.png")
+        ]
     }
 
-    exp2 = {
-        "title": "Accountant",
-        "company": "My Company",
-        "start_date": "October 2019",
-        "end_date": "Present",
-        "description": "Filing Taxes",
-        "logo": "example-logo.png"
-    }
+    expected_corr = [
+        {
+            'before': 'octber',
+            'after': 'october'
+        }
+    ]
 
-    exp3 = {
-        "title": "Lawyer",
-        "company": "A Cool Firm",
-        "start_date": "October 2020",
-        "end_date": "Present",
-        "description": "Case Studies",
-        "logo": "example-logo.png"
-    }
+    # Call spell_check function
+    corrections = spell_check(test_data["experience"])
 
-    ind1 = app.test_client().post('/resume/experience',
-                                  json=exp1).json['id']
-    ind2 = app.test_client().post('/resume/experience',
-                                  json=exp2).json['id']
-    ind3 = app.test_client().post('/resume/experience',
-                                  json=exp3).json['id']
-
-    # check if the correct index is returned
-    assert ind1 == 1
-    assert ind2 == 2
-    assert ind3 == 3
-
-    # check if GET request with index gives the same JSON
-    response = app.test_client().get('/resume/experience')
-    assert response.json[ind1] == exp1
-    assert response.json[ind2] == exp2
-    assert response.json[ind3] == exp3
-
-
-def test_experience():
-    '''
-    Add a new experience and then get all experiences. 
-    
-    Check that it returns the new experience in that list
-    '''
-    example_experience = {
-        "title": "Software Developer",
-        "company": "A Cooler Company",
-        "start_date": "October 2022",
-        "end_date": "Present",
-        "description": "Writing JavaScript Code",
-        "logo": "example-logo.png"
-    }
-
-    item_id = app.test_client().post('/resume/experience',
-                                     json=example_experience).json['id']
-    response = app.test_client().get('/resume/experience')
-    assert response.json[item_id] == example_experience
-
-
-def test_education():
-    '''
-    Add a new education and then get all educations. 
-    
-    Check that it returns the new education in that list
-    '''
-    example_education = {
-        "course": "Engineering",
-        "school": "NYU",
-        "start_date": "October 2022",
-        "end_date": "August 2024",
-        "grade": "86%",
-        "logo": "example-logo.png"
-    }
-    item_id = app.test_client().post('/resume/education',
-                                     json=example_education).json['id']
-
-    response = app.test_client().get('/resume/education')
-    assert response.json[item_id] == example_education
-
-
-def test_skill():
-    '''
-    Add a new skill and then get all skills. 
-    
-    Check that it returns the new skill in that list
-    '''
-    example_skill = {
-        "name": "JavaScript",
-        "proficiency": "2-4 years",
-        "logo": "example-logo.png"
-    }
-
-    item_id = app.test_client().post('/resume/skill',
-                                     json=example_skill).json['id']
-
-    response = app.test_client().get('/resume/skill')
-    assert response.json[item_id] == example_skill
+    assert corrections == expected_corr
